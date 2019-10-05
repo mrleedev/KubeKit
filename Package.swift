01a7a9579606fc4c13cb2e2ae60e5386973e230a
Lee Machin <me@mrl.ee>
2019-10-05 19:53:31 +0100

    Setup package manager

--- Package.swift
+++ Package.swift
@@ -1,23 +1,19 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KubeKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "KubeKit",
             targets: ["KubeKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "3.5.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KubeKit",
             dependencies: []),
