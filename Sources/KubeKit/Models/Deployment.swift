742dcffe0cf38ba278438371a66beb4178f83d0f
Lee Machin <me@mrl.ee>
2019-10-07 20:28:15 +0100

    Flatten structs to make linter happy

--- Sources/KubeKit/Models/Deployment.swift
+++ Sources/KubeKit/Models/Deployment.swift
@@ -4,58 +4,65 @@ struct Deployment: Codable {
     let kind: Kind
     let apiVersion: String
     let metadata: Metadata
-    let status: Status
-    let spec: Spec
-    
-    
-    struct Status: Codable {
-        let observedGeneration: Int
-        let replicas: Int
-        let updatedReplicas: Int
-        let availableReplicas: Int
-    }
-    
-    struct Spec: Codable {
-        let replicas: Int
-        let selector: LabelSelector
-        let template: Template
-        let strategy: Strategy
-        
-        struct Template: Codable {
-            let metadata: Metadata
-            
-            struct TemplateSpec: Codable {
-                let containers: [Container]
-                let restartPolicy: RestartPolicy
-                let terminationGracePeriodSeconds: Int
-                let dnsPolicy: DNSPolicy
-                let securityContext: [String: String]
-                
-                enum RestartPolicy: String, Codable {
-                    case always = "Always"
-                    case never = "Never"
-                }
-                
-                enum DNSPolicy: String, Codable {
-                    case clusterFirst = "ClusterFirst"
-                }
-                
-            }
-        }
-        
-        struct Strategy: Codable {
-            var type: StrategyType
-            var rollingUpdate: RollingUpdate?
-            
-            enum StrategyType: String, Codable {
-                case recreate = "Recreate"
-                case rollingUpdate = "RollingUpdate"
-            }
+    let status: DeploymentStatus
+    let spec: DeploymentSpec
+}
 
-            struct RollingUpdate: Codable {
-                var maxUnavailable: Int
-                var maxSurge: Int
-            }
-        }
+struct DeploymentSpec: Codable {
+    let replicas: Int
+    let selector: LabelSelector
+    let template: DeploymentSpecTemplate
+    let strategy: DeploymentSpecStrategy
+}
+
+struct DeploymentSpecTemplate: Codable {
+    let metadata: Metadata
+    let spec: DeploymentSpecTemplateSpec
+}
+
+struct DeploymentSpecTemplateSpec: Codable {
+    let containers: [Container]
+    let restartPolicy: RestartPolicy
+    let terminationGracePeriodSeconds: Int
+    let dnsPolicy: DNSPolicy
+    let securityContext: [String: String]
+
+    enum RestartPolicy: String, Codable {
+        case always = "Always"
+        case never = "Never"
     }
+
+    enum DNSPolicy: String, Codable {
+        case clusterFirst = "ClusterFirst"
+    }
+}
+
+struct DeploymentStatus: Codable {
+    let observedGeneration: Int
+    let replicas: Int
+    let updatedReplicas: Int
+    let availableReplicas: Int
+}
+
+enum DeploymentSpecStrategy: Codable {
+    case recreate(DeploymentStrategyRecreate)
+    case rollingUpdate(DeploymentStrategyRollingUpdate)
+
+    init(from decoder: Decoder) throws {
+        // todo
+    }
+
+    func encode(to encoder: Encoder) throws {
+        // todo
+    }
+}
+
+struct DeploymentStrategyRecreate: Codable {
+    let type: String
+}
+
+struct DeploymentStrategyRollingUpdate: Codable {
+    let type: String
+    let maxUnavailable: Int
+    let maxSurge: Int
 }
